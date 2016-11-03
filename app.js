3ce34557adc39a0e378082b7c86bacd26bc28c71
Jose Manuel Ortega Cerezo <joseo@iMac-de-Jose.local>
2016-11-03 15:12:48 +0100

    Intentos de conexion

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-var express = require("express")
-var app = express()
-var bodyParser = require("body-parser")
-var methodOverride = require("method-override")
-var mongoose = require("mongoose")
+var express  = require("express"),  
+    app      = express(),
+    http     = require("http"),
+    server   = http.createServer(app),
+    mongoose = require('mongoose');
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-app.use(methodOverride())
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());  
+app.use(methodOverride());
 
-var router = express.Router()
+var router = express.Router();
 
-router.get("/", function(req, res) {
-	res.send("http://localhost:3000")
-})
+router.get('/', function(req, res) {  
+   res.send("Hello World!");
+});
 
-app.use(router)
+app.use(router);
 
-app.listen(3000, function () {
-	console.log("Running on http://localhost:3000")
-})
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {  
+  if(err) {
+    console.log('ERROR: connecting to Database. ' + err);
+  }
+  app.listen(3000, function() {
+    console.log("Node server running on http://localhost:3000");
+  });
+});
