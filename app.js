4f6dc0d6104e59251f1d8f0e935c78d1c7c4dae4
Jose Manuel Ortega Cerezo <joseo@iMac-de-Jose.local>
2016-11-03 16:00:29 +0100

    Mas intentos de conexión

--- app.js
+++ app.js
@@ -1,26 +1,50 @@
-var express  = require("express"),  
-    app      = express(),
-    http     = require("http"),
-    server   = http.createServer(app),
-    mongoose = require('mongoose');
+var express = require("express")
+var app = express()
+var http = require("http")
+var server = http.createServer(app)
+var bodyParser = require('body-parser')
+var methodOverride = require('method-override')
+var mongoose = require('mongoose')
 
-app.use(bodyParser.urlencoded({ extended: false }));  
-app.use(bodyParser.json());  
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(methodOverride());
 
 var router = express.Router();
 
-router.get('/', function(req, res) {  
-   res.send("Hello World!");
+router.get('/', function (req, res) {
+	res.send("Hello World!");
 });
 
 app.use(router);
 
-mongoose.connect('mongodb://localhost/tvshows', function(err, res) {  
-  if(err) {
-    console.log('ERROR: connecting to Database. ' + err);
-  }
-  app.listen(3000, function() {
-    console.log("Node server running on http://localhost:3000");
-  });
+mongoose.connect('mongodb://localhost/tvshows', function (err, res) {
+	if (err) {
+		console.log('ERROR: connecting to Database. ' + err);
+	}
+	app.listen(3000, function () {
+		console.log("Node server running on http://localhost:3000");
+	});
+});
+mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+	res.send("Hello World!");
+});
+
+app.use(router);
+
+mongoose.connect('mongodb://localhost/tvshows', function (err, res) {
+	if (err) {
+		console.log('ERROR: connecting to Database. ' + err);
+	}
+	app.listen(3000, function () {
+		console.log("Node server running on http://localhost:3000");
+	});
 });
