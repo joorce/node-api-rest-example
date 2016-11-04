4db7f14756f29dacda81390ff2177cd765de4754
Jose Manuel Ortega Cerezo <joseo@iMac-de-Jose.local>
2016-11-04 08:47:39 +0100

    Inicio de API

--- app.js
+++ app.js
@@ -5,46 +5,32 @@ var server = http.createServer(app)
 var bodyParser = require('body-parser')
 var methodOverride = require('method-override')
 var mongoose = require('mongoose')
+var TVShorCtrl = require('./controllers/tvshows')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());  
 app.use(methodOverride());
 
 var router = express.Router();
 
-router.get('/', function (req, res) {
-	res.send("Hello World!");
+router.get('/', function(req, res) {  
+   res.send("Hello World!");
 });
 
 app.use(router);
 
-mongoose.connect('mongodb://localhost/tvshows', function (err, res) {
-	if (err) {
-		console.log('ERROR: connecting to Database. ' + err);
-	}
-	app.listen(3000, function () {
-		console.log("Node server running on http://localhost:3000");
-	});
-});
-mongoose = require('mongoose');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(methodOverride());
-
-var router = express.Router();
-
-router.get('/', function (req, res) {
-	res.send("Hello World!");
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {  
+  if(err) {
+    console.log('ERROR: connecting to Database. ' + err);
+  }
+  app.listen(3000, function() {
+    console.log("Node server running on http://localhost:3000");
+  });
 });
 
-app.use(router);
+var tvshows = express.Router()
 
-mongoose.connect('mongodb://localhost/tvshows', function (err, res) {
-	if (err) {
-		console.log('ERROR: connecting to Database. ' + err);
-	}
-	app.listen(3000, function () {
-		console.log("Node server running on http://localhost:3000");
-	});
-});
+tvshows.route('/tvshows')
+  .get(TVShorCtrl.findAllTVShows)
+
+app.use('/api', tvshows)
